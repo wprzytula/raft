807408720cc8992be642d707ce2436b9f1c58945
Wojciech Przytuła <wp418383@students.mimuw.edu.pl>
2022-01-06 15:19:10 +0100

    initial

---
+++ solution/src/lib.rs
@@ -0,0 +1,52 @@
+use std::time::SystemTime;
+
+use executor::{Handler, ModuleRef, System};
+
+pub use domain::*;
+
+mod domain;
+
+pub struct Raft {
+    config: ServerConfig,
+    state_machine: Box<dyn StateMachine>,
+    stable_storage: Box<dyn StableStorage>,
+    message_sender: Box<dyn RaftSender>,
+    // TODO you can add fields to this struct.
+}
+
+impl Raft {
+    /// Registers a new `Raft` module in the `system`, initializes it and
+    /// returns a `ModuleRef` to it.
+    pub async fn new(
+        system: &mut System,
+        config: ServerConfig,
+        first_log_entry_timestamp: SystemTime,
+        state_machine: Box<dyn StateMachine>,
+        stable_storage: Box<dyn StableStorage>,
+        message_sender: Box<dyn RaftSender>,
+    ) -> ModuleRef<Self> {
+        todo!();
+        system.register_module(Self {
+            config,
+            state_machine,
+            stable_storage,
+            message_sender
+        })
+    }
+}
+
+#[async_trait::async_trait]
+impl Handler<RaftMessage> for Raft {
+    async fn handle(&mut self, msg: RaftMessage) {
+        todo!()
+    }
+}
+
+#[async_trait::async_trait]
+impl Handler<ClientRequest> for Raft {
+    async fn handle(&mut self, msg: ClientRequest) {
+        todo!()
+    }
+}
+
+// TODO you can implement handlers of messages of other types for the Raft struct.
